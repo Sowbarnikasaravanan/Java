485235b9b86c7dd41d6b7fbb972fdccf06e3b8e0
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-21 20:03:01 +0530

    Addition of two numbers in java

---
+++ Addition.java
@@ -0,0 +1,14 @@
+// Addition of two numbers in java
+import java.io.*;
+import java.util.*;
+class Addition{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println ("Enter first number:");
+    int num1=sc.nextInt();
+    System.out.println("Enter second number:");
+    int num2=sc.nextInt();
+    int sum=num1+num2;
+    System.out.println("The sum of "+num1+" and "+num2+" is "+sum);
+  }
+}
