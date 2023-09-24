6c9b0ecf6ad6e38eeb7fbecdbb1fd1b4330a23d7
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-09-24 11:56:07 +0530

    To display the uppercase alphabets

---
+++ displayalpha.java
@@ -0,0 +1,9 @@
+//To  display all the alphabets
+import java.io.*;
+class displayalpha{
+  public static void main(String args[]){
+  for(char i='A';i<='Z';i++){
+      System.out.print(i+" ");
+  }
+  }
+}
