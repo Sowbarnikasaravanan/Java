e5398a4713a1f0e60c9b8b55f3b235989a87e7fd
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-11 23:19:49 +0530

    To perform transpose of matrix

---
+++ Matrixtranspose.java
@@ -0,0 +1,33 @@
+//To perform transpose of a matrix 
+import java.util.*;
+public class MatrixTranspose{
+    public static void main(String args[]){
+        int row,col;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the row :");
+        row=sc.nextInt();
+        System.out.println("Enter the size of the column:");
+        col=sc.nextInt();
+        int[][] a=new int[row][col];
+        System.out.println("Enter the elements of the matrix:");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("The elements of the matrix :");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("The transpose matrix is :");
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+                System.out.print(a[j][i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
