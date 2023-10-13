25d4098849c72d8b3028d3dd361ec5f524992a9c
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-13 23:35:03 +0530

    Create Invertedtriangle.java

---
+++ Invertedtriangle.java
@@ -0,0 +1,16 @@
+// To display the patterns in the inverted right angled triangle 
+import java.util.*;
+class InvertedTriangle{
+    public static void main(String args[]){
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        num=sc.nextInt();
+        for(int i=num;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
