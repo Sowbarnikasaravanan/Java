73f0ecdb70f364388b19d624cacd4b6a562595e6
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-22 21:16:43 +0530

    To count the number of characters in a string

--- Countcharacter.java
+++ Countcharacter.java
@@ -1,1 +1,17 @@
-/
+//To count the number of characters present in a string 
+import java.util.*;
+class CountCharacters{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string :");
+        String str=sc.nextLine();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z')){
+                count++;
+            }
+        }
+        System.out.println("The number of characters are :"+count);
+    }
+}
