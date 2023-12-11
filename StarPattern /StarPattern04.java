49dcfbaf343b6137da888308204cbbd6de97199a
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-12-11 13:56:43 +0530

    Create StarPattern04.java

---
+++ StarPattern /StarPattern04.java
@@ -0,0 +1,24 @@
+/* To display the Star pattern 
+    *
+   **
+  ***
+ ****
+*****
+*/
+import java.util.*;
+public class StarPattern03{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
