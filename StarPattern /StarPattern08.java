d687c916d4b3bf9ab5a391f72acb7d4294d3afa2
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-12-20 15:09:22 +0530

    Star Pattern 08

---
+++ StarPattern /StarPattern08.java
@@ -0,0 +1,27 @@
+/*  To display the star pattern
+*******
+*     *
+*     *
+*     *
+*     *
+*******
+*/
+import java.util.*;
+public class StarPattern08{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				if(i==0 || j==0 || i==n-1 || j==n-1){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
