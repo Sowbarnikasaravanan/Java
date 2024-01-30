c55b4ba4652f16988d79737d756fd0a8840e48da
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-30 22:35:32 +0530

    To print a number that is entered by an user

---
+++ PrintANumber.java
@@ -0,0 +1,16 @@
+// To print a number that is entered by an user
+import java.util.*;
+public class PrintANumber{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        System.out.println("You entered:"+n);
+    }
+}
+
+/* 
+Sample output:
+Enter a number:5
+You entered:5
+*/
