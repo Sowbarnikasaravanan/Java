a66c7c892eef71adb51eff0ff90aa94fa587a081
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-02-03 22:42:42 +0530

    Example for Typecasting

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+//Example for type casting
+public class Typecasting{
+    public static void main(String args[]){
+        int a=5;
+        double b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+/*
+Sample Output:
+5
+5.0
+*/
