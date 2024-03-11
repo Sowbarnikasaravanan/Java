0e1e859ba063a8e20ffd4db8ffb147e6b7e0c89c
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-03-11 23:26:05 +0530

    Star pattern 09

---
+++ StarPattern /Starpattern09.c
@@ -0,0 +1,24 @@
+/* 
+To display the star pattern 
+     *
+    * *
+   * * *
+  * * * *
+*/
+import java.util.*;
+public class Starpatter09{
+     public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the number:");
+         int n=sc.nextInt();
+         for(int i=1;i<=n;i++){
+             for(int j=1;j<=n-i;j++){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+     }
+}
