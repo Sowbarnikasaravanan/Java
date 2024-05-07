220c5b514ce19aa33a56817b2b739fc347348b31
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-05-07 22:32:18 +0530

    To put hash before the vowels

--- HashBeforeVowel.java
+++ HashBeforeVowel.java
@@ -1,6 +1,6 @@
 // To place '#' before vowels in a string
 import java.util.*;
-public class HashBeforeVowels{
+public class HashBeforeVowel{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the string:");
